a4581a4da4b5e1d8cebf3d72ceee2513577608c0
omkarpawar018 <110762365+omkarpawar018@users.noreply.github.com>
2024-08-01 13:11:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+smooth: true
+});
+
+var elemc = document.querySelector("#elem-container");
+
+var fixed = document.querySelector("#fixed-image");
+
+elemc.addEventListener("mouseenter", function() {
+    fixed.style.display = "block";
+});
+
+elemc.addEventListener("mouseleave", function() {
+    fixed.style.display = "none"; // Change this to hide the element on mouse leave
+});
+
+var elems=document.querySelectorAll(".elem");
+elems.forEach(function(e){
+    e.addEventListener("mouseenter",function(){
+        var image=e.getAttribute("data-image")
+        fixed.style.backgroundImage=`url(${image})`
+    })
+})
